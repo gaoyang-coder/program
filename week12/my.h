9314f6e567b25455dd8719b7802c2779db62dc11
gaoyang-coder <1050578122@qq.com>
2019-11-25 21:17:17 -0800

    12months

---
+++ week12/my.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <sys/syscall.h>
+#include <errno.h>
+#include <signal.h>
+#include <time.h>
